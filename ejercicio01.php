52efbaffa137ab845c46964d725fa44a5a78756e
MiguelLeci <miguel.leci.leci@gmail.com>
2016-09-30 18:35:26 +0200

    30/09/16

--- ejercicio01.php
+++ ejercicio01.php
@@ -1,1 +1,24 @@
-TODO:
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ejercicio1</title>
+    </head>
+    <body>
+        <?php
+            $element = "";
+            $array1[] = "a";
+            $array1[] = "b";
+            $array1[] = "c";
+            $array1[] = "d";
+            $array1[] = "e";
+
+            for ($num = 0;$num <= 5 ;$num++) {
+                $element = $array1[$num] ;
+                echo "$element";
+            }
+
+
+        ?>
+    </body>
+</html>
